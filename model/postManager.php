56814c58c9b33f6d3d414f3737f9209dadb23adc
ludovic.zibelli <ludovic.zibelli@free.fr>
2019-02-22 15:29:46 +0100

    Ajout d'une nouvelle fonction uploadPicture pour le telechargement des image sur le serveur

--- model/postManager.php
+++ model/postManager.php
@@ -51,12 +51,46 @@ class PostManager extends Manager
 	}
 
 	//Ajouter un billet
-	public function addPost($title, $content,$chapitre,$online)
+	public function addPost($title, $content,$chapitre,$online,$picture)
 	{
 		$db = $this->dbConnect();
-		$req = $db->prepare('INSERT INTO posts(title, content, chapitre, creation_date , modification_date, online) VALUES (?,?,?,NOW(),NOW(),?)');
-		$affectLine = $req->execute(array($title,$content,$chapitre,$online));
+		$req = $db->prepare('INSERT INTO posts(title, content, chapitre, creation_date , modification_date, online, pictures) VALUES (?,?,?,NOW(),NOW(),?,?)');
+		$affectLine = $req->execute(array($title,$content,$chapitre,$online,$picture));
 		return $affectLine;
 	}
+
+
+	public function uploadPicture()
+	{
+
+
+			$maxsize = 100000;
+			$image = $_FILES['image']['name'];
+			$extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
+			//1. strrchr renvoie l'extension avec le point (« . »).
+			//2. substr(chaine,1) ignore le premier caractère de chaine.
+			//3. strtolower met l'extension en minuscules.
+			$extension_upload = strtolower(  substr(  strrchr($_FILES['image']['name'], '.')  ,1)  );
+			if ( in_array($extension_upload,$extensions_valides) )
+			{
+				
+				if ($_FILES['image']['size'] > $maxsize) 
+					{
+						$erreur = "Le fichier est trop gros";
+					}
+					else
+					{
+						$nom = "public/images/upload/{$_FILES['image']['name']}";
+						$resultat = move_uploaded_file($_FILES['image']['tmp_name'],$nom);
+						if ($resultat) 
+						{
+							return $image;
+						}
+					}
+			}
+		
+		
+			
+		}	
 
 }
