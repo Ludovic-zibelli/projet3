35da579daf7621811d864a79ca3c660a2f177515
ludovic.zibelli <ludovic.zibelli@free.fr>
2019-02-25 22:46:53 +0100

    Mise en forme des lables + ajout d'un lien mot de pass oublier

--- view/frontend/userView.php
+++ view/frontend/userView.php
@@ -20,19 +20,16 @@ if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
 				<div class="row">
 				<div class="col-lg-4"></div>
 				<form action="/projet3/index.php?action=connexion" method="POST" class="form-signin col-lg-4" style="float: none;">
-							
-							<label for="user" class="sr-only">Identifiant : </label>
+							<fieldset>
+							<label for="user" class="col-form-label list-inline-item">Identifiant : </label>
 							<input type="text" name="user" class="form-control" placeholder="Identifiant"><br>
-							<label for="pass" class="sr-only">Mot de passe : </label>
+							<label for="pass" class="col-form-label list-inline-item">Mot de passe : </label>
 							<input type="password" name="pass" class="form-control" placeholder="Mot de passe"><br>
-							
-							
 							<input type="submit" name="connexion" value="Connexion" class="btn btn-lg btn-primary btn-block">
-						
-					
-					
+							<a class="col-form-label list-inline-item" href="">Mot de passe oublier ?</a>
+							</fieldset>
 				</form>
-
+				
 				</div>
 			</div>
 			<div class="message_erreur"><p><?php if(isset($_GET['message_erreur'])){echo $_GET['message_erreur']; } ?></p></div>
