58888acd7d2dace722d4446f23a9148b9db637a1
ludovic.zibelli <ludovic.zibelli@free.fr>
2019-02-27 22:32:42 +0100

    Ajout d'une fonction pour la recuperation de mot de passe

--- controleur/frontend.php
+++ controleur/frontend.php
@@ -270,6 +270,86 @@ function connexion($pseudo, $pass)
 		
 		
 	}
+
+}
+
+function recupMail()
+{
+	$recup_mail = htmlspecialchars($_POST['recup_mail']);
+	if(filter_var($recup_mail,FILTER_VALIDATE_EMAIL))
+	{
+		$control_mail = new \openclassrooms\blog\model\userManager();
+		$resultat_recup = $control_mail->controlMail($recup_mail);
+		if($resultat_recup)
+		{
+			$pseudo = $resultat_recup['pseudo'];
+			$_SESSION['mail_recup'] = $recup_mail;
+			$recup_code = "";
+			for ($i=0; $i < 8; $i++) 
+			{ 
+				$recup_code .= mt_rand(0,9);
+			}
+			$recup_email = new \openclassrooms\blog\model\userManager();
+			$return_recup = $recup_email->recupMail($recup_mail);
+			if($return_recup == 1)
+			{
+				$update_mail = new \openclassrooms\blog\model\userManager();
+				$update_email = $update_mail->editRecup($recup_code,$recup_mail);
+
+			}
+			else
+			{
+				$inser_code = new \openclassrooms\blog\model\userManager();
+				$inserCode = $inser_code->inserRecup($recup_mail,$recup_code);
+
+			}
+				 $header="MIME-Version: 1.0\r\n";
+		         $header.='From:"PrimFX.com"<support@primfx.com>'."\n";
+		         $header.='Content-Type:text/html; charset="utf-8"'."\n";
+		         $header.='Content-Transfer-Encoding: 8bit';
+		         $message = '
+		         <html>
+		         <head>
+		           <title>Récupération de mot de passe</title>
+		           <meta charset="utf-8" />
+		         </head>
+		         <body>
+		           <font color="#303030";>
+		             <div align="center">
+		               <table width="600px">
+		                 <tr>
+		                   <td>
+		                     
+		                     <div align="center">Bonjour <b>'.$pseudo.'</b>,</div>
+		                     Voici votre code de récupération: <b>'.$recup_code.'</b>
+		                     A bientôt sur <a href="http://projet3.zibelliludovic.fr">Le blog </a> !
+		                     
+		                   </td>
+		                 </tr>
+		                 <tr>
+		                   <td align="center">
+		                     <font size="2">
+		                       Ceci est un email automatique, merci de ne pas y répondre
+		                     </font>
+		                   </td>
+		                 </tr>
+		               </table>
+		             </div>
+		           </font>
+		         </body>
+		         </html>
+		         ';
+		         mail($recup_mail, "Récupération de mot de passe - PrimFX.com", $message, $header);
+		}
+		else
+		{
+			$error = "Adresse mail invalide";
+		}
+	}
+	else
+	{
+		$error = "Veuillez entrer votre mail";
+	}
 }
 
 
