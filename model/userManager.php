88a5300774bcc1559d67996e545a4b7e8cd04151
ludovic.zibelli <ludovic.zibelli@free.fr>
2019-02-27 22:36:12 +0100

    Ajout de 4 fonction 1 pour controler si le mail et present dans la bdd 2 pour controler si le mail et prensent dans la table recuperation 3 edit du mail dans la table recuperation pour modifier uniquement le code 4 si pas present ajout du mail + du code pour la recuperation du mot de passe

--- model/userManager.php
+++ model/userManager.php
@@ -22,4 +22,43 @@ class userManager extends Manager
     	return $resultat;
    
 	}
+
+	public function controlMail($email)
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare('SELECT id, pseudo FROM user WHERE email = ? ');
+		$req->execute(array($email));
+		$resultat_recup = $req->fetch();
+
+		return $resultat_recup;
+	}
+
+	public function recupMail($email)
+	{
+		$db = $this->dbConnect(); 
+		$req = $db->prepare('SELECT id FROM recuperation WHERE mail = ? ');
+		$req->execute(array($email));
+		$recupMail = $req->rowCount();
+
+		return $recupMail;
+	}
+
+	public function editRecup($code,$mail)
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare('UPDATE recuperation SET code = ? WHERE mail = ?');
+		$req->execute(array($code,$mail));
+		$return_edit = $req;
+		return $return_edit;
+	}
+
+	public function inserRecup($mail,$code)
+	{
+		$db = $this->dbConnect();
+		$req = $db->prepare('INSERT INTO recuperation(mail,code) VALUES (?,?)');
+		$req->execute(array($mail,$code));
+		$return_inser = $req;
+		return $return_inser;
+
+	}
 }
